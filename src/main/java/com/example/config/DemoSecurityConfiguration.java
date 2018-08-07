b7b3eba2b157506de066ba5ea468fc30dc8123b6
Bkake <bangaly.kake@gmail.com>
2018-08-07 15:12:41 +0200

    DÃfinition de la stratÃgie de nommage des rÃles

--- src/main/java/com/example/config/DemoSecurityConfiguration.java
+++ src/main/java/com/example/config/DemoSecurityConfiguration.java
@@ -1,8 +1,13 @@
 package com.example.config;
 
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
@@ -13,12 +18,30 @@ public class DemoSecurityConfiguration {
     public static class KeycloakConfigurationAdapter extends KeycloakWebSecurityConfigurerAdapter {
 
         /**
-         *  Définition de la stratégie d'authentification de session.
-         *
-          */
+         * Définition de la stratégie d’authentification de session.
+         * @return
+         */
+        @Bean
         @Override
         protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
           return new NullAuthenticatedSessionStrategy();
+        }
+
+        /**
+         * Définition de la stratégie de nommage des rôles
+         *
+         * Enregistre le KeycloakAuthenticationProvider avec
+         * le gestionnaire d'authentification.
+         *
+         * @param auth
+         * @throws Exception
+         */
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+            KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+            // simple mappeur d'autorité pour éviter ROLE_
+            keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+            auth.authenticationProvider(keycloakAuthenticationProvider);
         }
     }
 }
